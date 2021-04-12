c4e64f83047ce37f3faaee8e4901a019f1226fdd
JD Soteldo <jdsoteldo00@gmail.com>
2021-04-12 10:42:17 -0400

    some structural changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,11 @@ Rails.application.routes.draw do
   root to: "invoices#index"
 
   resources :users do
+    resources :businesses, except: [:index]
+  end
+
+  resources :businesses do
     resources :invoices, except: [:index]
   end
 
-  resources :users do
-    resources :businesses
-  end
 end
-
-
-# resources :users do
-  # resources :businesses do
-    # resources :invoices, except: [:index]
-  # end
-# end
